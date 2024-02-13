a15db4dc994b1b784f870f7a4f71483974372a41
Ade0luwa <147874098+Ade0luwa@users.noreply.github.com>
2024-02-12 19:04:24 -0500

    Add project files.

---
+++ TestClass/Class1.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using NUnitDemonstration;
+
+namespace TestClass
+{
+    public class Class1
+    {
+        [Test]
+
+        public void TriangleTestOutput()
+        {
+            //arrange
+            int firstAngle = 60;
+            int secondAngle = 60;
+            int thirdAngle = 60;
+
+            string expected = "The triangle is valid.";
+            // Adegbesan Adeoluwatomiwa wrote this code
+
+            //act
+
+            string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+
+            //assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ class1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml.Linq;
+
+namespace NUnitDemonstration
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int thirdAngle)
+        {
+            // Adegbesan Adeoluwatomiwa wrote this code
+
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180) // changed the operator signs
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+}
